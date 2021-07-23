374a581609d973ca134e04df2216402c055eb4c2
Mauricio Adrián Piñón Islas <mauricio_2612@live.com.mx>
2021-07-23 02:30:26 -0500

    Se añadió y configuró ReactHelmet al proyecto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-
+import {Helmet} from "react-helmet";
 import AwesomeCarousel from './components/AwesomeCarousel'
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+       <Helmet>
+        <meta charSet="utf-8" />
+        <title>Crypto Gallery Art</title>
+        <link rel="canonical" href="https://crypto-gallery-art.web.app/" />
+      </Helmet>
       <div id="PageContainer">
       <Navbar/>
       <main >
